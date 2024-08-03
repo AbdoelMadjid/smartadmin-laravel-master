593ea5004076dd7b1586aa87317e2b2ee050e108
abdoelmadjid <abdulmadjid.mpd@gmail.com>
2024-08-03 13:27:48 +0700

    update 03 Agust 2024

--- database/seeders/MenuSeeder.php
+++ database/seeders/MenuSeeder.php
@@ -21,10 +21,25 @@ class MenuSeeder extends Seeder
         /**
          * @var Menu $mm
          */
-        $mm = Menu::firstOrCreate(['url' => 'konfigurasi'], ['name' => 'Konfigurasi', 'category' => 'MASTER DATA', 'icon' => 'settings']);
+        $mm = Menu::firstOrCreate(['url' => 'konfigurasi'], ['name' => 'Konfigurasi', 'category' => 'MASTER DATA', 'icon' => 'cog']);
         $this->attachMenupermission($mm, ['read'], ['admin']);
 
         $sm = $mm->subMenus()->create(['name' => 'Menu', 'url' => $mm->url . '/menu', 'category' => $mm->category]);
         $this->attachMenupermission($sm, ['create', 'read', 'update', 'delete', 'sort'], ['admin']);
+
+        $sm = $mm->subMenus()->create(['name' => 'Role', 'url' => $mm->url . '/roles', 'category' => $mm->category]);
+        $this->attachMenupermission($sm, ['create', 'read', 'update', 'delete'], ['admin']);
+
+        $sm = $mm->subMenus()->create(['name' => 'Permission', 'url' => $mm->url . '/permissions', 'category' => $mm->category]);
+        $this->attachMenupermission($sm, ['create', 'read', 'update', 'delete'], ['admin']);
+
+        $sm = $mm->subMenus()->create(['name' => 'Akses Role', 'url' => $mm->url . '/akses-role', 'category' => $mm->category]);
+        $this->attachMenupermission($sm, ['read', 'update'], ['admin']);
+
+        $sm = $mm->subMenus()->create(['name' => 'Akses User', 'url' => $mm->url . '/akses-user', 'category' => $mm->category]);
+        $this->attachMenupermission($sm, ['read', 'update'], ['admin']);
+
+        $sm = $mm->subMenus()->create(['name' => 'Users', 'url' => $mm->url . '/users', 'category' => $mm->category]);
+        $this->attachMenupermission($sm, null, ['admin']);
     }
 }
