7c924afb818272b56237f6a5fba9dd8656402f16
AbdoelMadjid <abdulmadjid.mpd@gmail.com>
2024-08-22 14:10:57 +0700

    update 22 Agust 2024

--- app/Models/Permission.php
+++ app/Models/Permission.php
@@ -2,10 +2,6 @@
 
 namespace App\Models;
 
-<<<<<<< HEAD
-=======
-use App\Models\Konfigurasi\Menu;
->>>>>>> 4656ac1d484d792549066f2657c52872817354e6
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Spatie\Permission\Models\Permission as ModelsPermission;
@@ -13,12 +9,4 @@ use Spatie\Permission\Models\Permission as ModelsPermission;
 class Permission extends ModelsPermission
 {
     use HasFactory;
-<<<<<<< HEAD
-=======
-
-    public function menus()
-    {
-        return $this->belongsToMany(Menu::class);
-    }
->>>>>>> 4656ac1d484d792549066f2657c52872817354e6
 }
--- database/seeders/MenuSeeder.php
+++ database/seeders/MenuSeeder.php
@@ -3,21 +3,12 @@
 namespace Database\Seeders;
 
 use App\Models\Konfigurasi\Menu;
-<<<<<<< HEAD
-=======
-use App\Models\Permission;
-use App\Traits\HasMenuPermission;
->>>>>>> 4656ac1d484d792549066f2657c52872817354e6
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Cache;
 
 class MenuSeeder extends Seeder
 {
-<<<<<<< HEAD
-=======
-    use HasMenuPermission;
->>>>>>> 4656ac1d484d792549066f2657c52872817354e6
     /**
      * Run the database seeds.
      */
@@ -27,31 +18,8 @@ class MenuSeeder extends Seeder
         /**
          * @var Menu $mm
          */
-<<<<<<< HEAD
         $mm = Menu::firstOrCreate(['url' => 'konfigurasi'], ['name' => 'Konfigurasi', 'category' => 'Intel Application', 'icon' => 'settings']);
 
         $sm = $mm->subMenus()->create(['name' => 'Menu', 'url' => $mm->url . '/menu', 'category' => $mm->category]);
-=======
-        $mm = Menu::firstOrCreate(['url' => 'konfigurasi'], ['name' => 'Konfigurasi', 'category' => 'MASTER DATA', 'icon' => 'cog']);
-        $this->attachMenupermission($mm, ['read'], ['admin']);
-
-        $sm = $mm->subMenus()->create(['name' => 'Menu', 'url' => $mm->url . '/menu', 'category' => $mm->category]);
-        $this->attachMenupermission($sm, ['create', 'read', 'update', 'delete', 'sort'], ['admin']);
-
-        $sm = $mm->subMenus()->create(['name' => 'Role', 'url' => $mm->url . '/roles', 'category' => $mm->category]);
-        $this->attachMenupermission($sm, ['create', 'read', 'update', 'delete'], ['admin']);
-
-        $sm = $mm->subMenus()->create(['name' => 'Permission', 'url' => $mm->url . '/permissions', 'category' => $mm->category]);
-        $this->attachMenupermission($sm, ['create', 'read', 'update', 'delete'], ['admin']);
-
-        $sm = $mm->subMenus()->create(['name' => 'Akses Role', 'url' => $mm->url . '/akses-role', 'category' => $mm->category]);
-        $this->attachMenupermission($sm, ['read', 'update'], ['admin']);
-
-        $sm = $mm->subMenus()->create(['name' => 'Akses User', 'url' => $mm->url . '/akses-user', 'category' => $mm->category]);
-        $this->attachMenupermission($sm, ['read', 'update'], ['admin']);
-
-        $sm = $mm->subMenus()->create(['name' => 'Users', 'url' => $mm->url . '/users', 'category' => $mm->category]);
-        $this->attachMenupermission($sm, null, ['admin']);
->>>>>>> 4656ac1d484d792549066f2657c52872817354e6
     }
 }
